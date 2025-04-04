5b54f1c006f7c41e98428a2f75e1e37c39c42ef0
Pawan Kapoor <kapoorpawan7390@gmail.com>
2025-04-04 10:19:17 +0530

    Initial commit for Python Web API

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from models import Item
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, world from Python API!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"received_item": item}
