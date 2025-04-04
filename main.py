09255c21262bdb5805590057df32b52530b637ac
Pawan Kapoor <kapoorpawan7390@gmail.com>
2025-04-04 16:05:31 +0530

    main swagger

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import uvicorn
 from fastapi import FastAPI
 from models import Item
 
-app = FastAPI()
+app = FastAPI(
+    title="Python Web API with Swagger",
+    description="This is a simple API built with FastAPI and deployed using Jenkins.",
+    version="1.0.0",
+    docs_url="/docs",             # Swagger UI
+    redoc_url="/redoc",           # ReDoc UI
+    openapi_url="/openapi.json"   # OpenAPI schema
+)
 
 @app.get("/")
 def read_root():
@@ -18,8 +25,3 @@ def read_item(item_id: int, q: str = None):
 def create_item(item: Item):
     return {"received_item": item}
 
-#ccommentfg
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Use PORT from Azure
-    uvicorn.run(app, host="0.0.0.0", port=port)
-    
