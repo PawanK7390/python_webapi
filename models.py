31df9266f7815a5cbf3125c14928148d22dd0d57
Pawan Kapoor <kapoorpawan7390@gmail.com>
2025-04-04 12:18:06 +0530

    model

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 class Item(BaseModel):
     name: str
-    price: float
     description: str = None
-    in_stock: bool = True
+    price: float
+    is_offer: bool = None
